fb0a14d41f01f3e0dba725ae734bbd2286acd6af
KubaBoi <jakubanderle@outlook.cz>
2022-08-22 22:10:07 +0200

    debug

---
+++ README.md
@@ -0,0 +1,7 @@
+# FTP server
+
+## Install
+
+python -m pip install -U pip setuptools
+
+pip install pyftpdlib
--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
-from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib import servers
 from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
-
-def runFileServer():
-    authorizer = DummyAuthorizer()
-
-    authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
-    authorizer.add_anonymous(os.getcwd())
-
-    handler = FTPHandler
-    handler.authorizer = authorizer
-
-    handler.banner = "pyftpdlib based ftpd ready."
-
-    address = ('0.0.0.0', 21)
-    server = FTPServer(address, handler)
-
-    server.max_cons = 256
-    server.max_cons_per_ip = 5
-
-    server.serve_forever()
-
-
-if __name__ == "__main__":
-    runFileServer()
+address = ("0.0.0.0", 21)  # listen on every IP on my machine on port 21
+server = servers.FTPServer(address, FTPHandler)
+server.serve_forever()
