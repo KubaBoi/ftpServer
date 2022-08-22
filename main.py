ede1121de7b44324d6e0fb9cbfa78d9a977822b4
KubaBoi <jakubanderle@outlook.cz>
2022-08-22 23:16:12 +0200

    debug

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyftpdlib.servers import FTPServer
 def runFileServer():
     authorizer = DummyAuthorizer()
 
-    authorizer.add_user('kuba', 'Heslo_12', '/', perm='elradfmwMT')
+    authorizer.add_user('kuba', 'Heslo_12', '/..', perm='elradfmwMT')
     authorizer.add_anonymous(os.getcwd())
 
     handler = FTPHandler
