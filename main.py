f49f059da5391e192d1b9493528f06b508a90de4
KubaBoi <jakubanderle@outlook.cz>
2022-08-22 22:00:00 +0200

    debug

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def runFileServer():
+    authorizer = DummyAuthorizer()
+
+    authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
+    authorizer.add_anonymous(os.getcwd())
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    address = ('0.0.0.0', 21)
+    server = FTPServer(address, handler)
+
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    runFileServer()
