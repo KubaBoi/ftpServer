e4e1ac6838b24b090d2e650a41c69c53f2950730
KubaBoi <jakubanderle@outlook.cz>
2022-08-22 23:34:37 +0200

    fbu

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def runFileServer():
     handler = FTPHandler
     handler.authorizer = authorizer
     handler.masquerade_address = '130.61.38.75'
-
+    handler.passive_ports = range(60000, 60099)
     handler.banner = "pyftpdlib based ftpd ready."
 
     address = ('', 21)
